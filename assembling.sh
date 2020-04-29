501ca8bccdf847f3c88bf8efd74ab70faf401a08
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-04-29 22:15:00 +0200

    version 1.0.1

--- assembling.sh
+++ assembling.sh
@@ -2,3 +2,5 @@
 
 ##acquire information about all subreads and filtered subreads in the dataset
 ls -lah ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/;
+#create correspnding repositories in the rawdata  directory
+ls ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/*|cut -d / -f 7|while read fungi_name;do mkdir ./rawdata/$fungi_name;done;
