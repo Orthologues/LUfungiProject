bf5ace9e3ceac5184e28805cc3dbaab92b894abd
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-04-29 20:49:28 +0200

    the bash script for genome assemling

---
+++ assembling.sh
@@ -0,0 +1,4 @@
+#This script records bash commands used for fungal genome assembly
+
+##acquire information about all subreads and filtered subreads in the dataset
+ls -lah ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/;
