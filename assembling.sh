2ea7a1803005e9e3d85293c5c993dab64d42d42b
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-04-30 22:01:15 +0200

    version 1.0.2

--- assembling.sh
+++ assembling.sh
@@ -2,5 +2,7 @@
 
 ##acquire information about all subreads and filtered subreads in the dataset
 ls -lah ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/;
-#create correspnding repositories in the rawdata  directory
+##create correspnding repositories in the rawdata  directory
 ls ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/*|cut -d / -f 7|while read fungi_name;do mkdir ./rawdata/$fungi_name;done;
+##Do fastqc analysis for the 4 given fastq.gz files
+ls fastqcReports/*|cut -d / -f 2|sed s/://g|while read name;do nohup fastqc --threads 20 -o ./fastqcReports// --extract -f fastq -c ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX//*reads/*.gz;done;
