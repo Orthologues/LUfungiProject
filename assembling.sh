13d22fdf2bc6cf9ec57452f564f016eb51f1a62a
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-05-03 22:47:18 +0200

    version 1.0.4

--- assembling.sh
+++ assembling.sh
@@ -2,9 +2,13 @@
 
 ##acquire information about all subreads and filtered subreads in the dataset
 ls -lah ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/;
-##create correspnding repositories in the fastqcReports  directory
+##create corresponding repositories in the fastqcReports directory
 ls ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/*|cut -d / -f 7|while read fungi_name;do mkdir ./fastqcReports/$fungi_name;done;
 ##Do fastqc analysis for the 4 given fastq.gz files
 ls fastqcReports/*|cut -d / -f 2|sed s/://g|while read name;do nohup fastqc --threads 20 -o ./fastqcReports/$name/ --extract -f fastq -c ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/$name/*reads/*.gz;done;
 #Do multiqc analysis from results of fastqcReports
 multiqc ./fastqcReports/ -o multiQCreport/;
+##From the supplementary material it can be learned that the genome sequencing and RNA sequencing were done by PacBio, so '--pacbio' option should be used
+##create corresponding repositories in the spadesAssembly directory
+ls ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/*|cut -d / -f 7|while read fungi_name;do mkdir ./spadesAssembly/$fungi_name;done;
+
