2e553c2d2e13d36db9ad89a440b95af0aac069ee
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-05-13 23:46:03 +0200

    version 1.0.5

--- assembling.sh
+++ assembling.sh
@@ -12,3 +12,12 @@ multiqc ./fastqcReports/ -o multiQCreport/;
 ##create corresponding repositories in the spadesAssembly directory
 ls ../../shared_bioinformatics_master_projects/agaricalesGenomes/b2016040/INBOX/*/*subreads/*|cut -d / -f 7|while read fungi_name;do mkdir ./spadesAssembly/$fungi_name;done;
 
+##Create .gitignore
+ls -d */|sed "s/\///g"|while read name;do echo $name >> .gitignore;done
+
+##Long PacBio RSII reads assembly by wtdbg2 
+nohup ./wtdbg2 -x rs -g 6.5g -i ../pb_279_filtered_subreads.fastq.gz -t 20 -fo ../wtdbg2Assembly/pb_279_default/pb_279_default
+nohup ./wtdbg2 -x rs -g 6.9g -i ../pb_320-2_filtered_subreads.fastq.gz -t 20 -fo ../wtdbg2Assembly/pb_320-2_default/pb_320-2_default
+
+##In order to convert original .fastq.gz files of short reads into .BAM files for genome polishing with arrow, it's necessary to first use bowtie2 to have SAM format which can be transformed to BAM format with Samtools then
+ 
