7b643c44529b49ba0d576fccbcb548bdcb92ed10
Master student working with Dag Ahrén <jiawei_zhao@memeg-beha5b.biol.lu.se>
2020-06-18 23:03:30 +0200

    Integrated pipeline scripts ver0.9

--- pb-assembly/integrated_busco.sh
+++ pb-assembly/integrated_busco.sh
@@ -6,7 +6,7 @@ helpFunction()
    echo "An integrated pipeline shell script to do busco analysis for falcon-assemblies of different steps after running integrated_falcon_quast.sh"
    echo -e "\t-r [STR] Specifies the parent directory of your 'pb-assembly' directory"
    echo -e "\t-n [STR] Specifies the index of your species in PacBio sequencing. For example, if you input '279', its corresponding string would be 'pb_279'"
-   echo -e "\t-k [STR] Specifies the kingdom name of your busco dataset. For example, '${parK}'\n"
+   echo -e "\t-k [STR] Specifies the kingdom name of your busco dataset. For example, 'fungi_odb10'\n"
    echo "Disclaimer: You must have installed conda and have run these commands before using this script:"
    echo -e "\tconda create -n busco -c bioconda -c conda-forge busco=4.0.6 python=3.7"
    echo -e "\tconda deactivate"
